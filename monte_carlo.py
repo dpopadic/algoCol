d6eb1a757b457a8a8e2b354209722f700d59851c
dpopadic <dario.popadic@yahoo.com>
2020-05-15 23:42:32 +0100

    monte carlo prototype

---
+++ monte_carlo.py
@@ -0,0 +1,17 @@
+# monte carlo prototype -----------------------------------------------------------------------------------------------
+# intro ----------------
+# When one has a generative model of a domain but no analytical solution, one uses monte-carlo simulation. It's a
+# class of techniques using random sampling a draw from a distribution. Solutions for problems can be calculated
+# using repeated draws and aggregating the results.
+# 1. create domain model
+# 2. generate random draws from the distribution
+# 3. deterministic output calculation
+# 4. compute aggregate statistics
+import pandas as pd
+import numpy as np
+
+# simulation: roll pair of dice ----------------
+
+
+
+
